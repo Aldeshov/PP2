3de8943a1e93d19718b42df81013d01b333e5ed8
Aldeshov <azat.aldeshov@gmail.com>
2019-01-31 01:11:29 +0600

    Ok

---
+++ Week1/Task3/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] s = Convert.ToString(Console.ReadLine()).Split();
+            int[] a = new int[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(s[i]);
+            }
+            for (int i = 0; i < n; i++)
+            {
+            Console.Write(a[i] + " ");
+            Console.Write(a[i] + " ");
+            }
+
+        }
+    }
+}
