c5b1de1b76aba1d411db8b5ec5e9bfdd0b120c61
Aldeshov <azat.aldeshov@gmail.com>
2019-01-30 23:48:13 +0600

    “Task1”

---
+++ Week1/Task1/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[n];
+            string[] arr = Console.ReadLine().Split();
+            for(int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(arr[i]);
+
+                if (a[i] % 2 != 0)
+                    Console.Write(a[i] + " ");
+            }
+            
+
+        }
+    }
+}
