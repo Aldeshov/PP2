71e1a0fced5ce64d5b792370ab6ac319d08e50fb
Aldeshov <azat.aldeshov@gmail.com>
2019-01-31 00:58:45 +0600

    Ok

---
+++ Week1/Task2/Task2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Ex7
+{
+    class Student
+    {
+        public string name;
+        public string id;
+        public int year;
+
+        public Student(int year, string name, string id)
+        {
+            this.name = name;
+            this.id = id;
+            this.year = year;
+        }
+
+     
+        public override string ToString()
+        {
+            return name + " " + id + " " + year;
+        }
+
+    }
+
+    class Program
+    {
+        static void StudentInfo()
+        {
+            string name = Convert.ToString(Console.ReadLine());
+            string id = Convert.ToString(Console.ReadLine());
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Student s = new Student(year,name,id);
+            Console.WriteLine(s.ToString());
+        }
+        
+        static void Main(string[] args)
+        {
+            StudentInfo();
+        }
+    }
+}
