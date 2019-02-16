8054453efa01c1009f0052645a09a3499fef3ac3
Aldeshov <azat.aldeshov@gmail.com>
2019-02-16 08:15:08 +0600

    Ok

--- Week1/Task3/Task3/Program.cs
+++ Week1/Task3/Task3/Program.cs
@@ -13,16 +13,18 @@ namespace Task3
             int n = Convert.ToInt32(Console.ReadLine());// Read numbers From User
             string[] s = Convert.ToString(Console.ReadLine()).Split(); // All Numbers will be in array s[](string)
             int[] a = new int[n]; // and New array for Numbers a[](integer)
-
+            int[] b = new int[n * 2]; // New array for Double numbers
             for(int i = 0; i < n; i++)
             {
                 a[i] = int.Parse(s[i]); // Convert Every element os s[string] to a[integer]
             }
             for (int i = 0; i < n; i++)
             {
-            Console.Write(a[i] + " "); // Write Current Element
-            Console.Write(a[i] + " "); // Double
+                b[i * 2] = a[i];
+                b[i * 2 + 1] = a[i];
             }
+            for (int i = 0; i < b.Length; i++)
+                Console.Write(b[i] + " "); // Write Double array
 
         }
     }
